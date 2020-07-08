d2ddecb06286f6155f5364f23a5621636409a766
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-08 13:44:46 -0400

    Implemented Map View and Camera Functionalities

--- PhotoMap/View Controllers/PhotoMapViewController.m
+++ PhotoMap/View Controllers/PhotoMapViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "PhotoMapViewController.h"
+#import <MapKit/MapKit.h>
 
 @interface PhotoMapViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, weak) UIImageView *pictureChosen;
 
 @end
 
@@ -16,12 +20,77 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    MKCoordinateRegion sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667), MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:sfRegion animated:false];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) pictureSource{
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle: @"Camera"
+           message:@"Choose Source"
+    preferredStyle:(UIAlertControllerStyleActionSheet)];
+    
+    UIAlertAction *cameraSource = [UIAlertAction actionWithTitle:@"Camera"
+                                                           style: UIAlertActionStyleDefault
+                                                           handler:^(UIAlertAction * _Nonnull action) {
+                                                           if ([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeCamera]) {
+                                                               imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+                                                           }
+                                                           else {
+                                                               NSLog(@"Camera 🚫 available so we will use photo library instead");
+                                                               imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                                                               
+                                                           }
+                                                          [self presentViewController: imagePickerVC animated:YES completion:nil];
+    }];
+    
+    UIAlertAction *librarySource = [UIAlertAction actionWithTitle: @"Photo Library"
+                                                          style: UIAlertActionStyleDefault
+                                                          handler:^(UIAlertAction * _Nonnull action) {
+                                                          imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                                                          [self presentViewController: imagePickerVC animated:YES completion:nil];
+    }];
+    
+    [alert addAction: cameraSource];
+    [alert addAction: librarySource];
+    
+    [self presentViewController: alert animated:YES completion:^{}];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    self.pictureChosen.image = [self resizeImage: editedImage withSize: CGSizeMake(414, 414)];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)cameraButton:(id)sender {
+    [self pictureSource];
+    [self performSegueWithIdentifier: @"tagSegue" sender: nil];
 }
 
 /*
